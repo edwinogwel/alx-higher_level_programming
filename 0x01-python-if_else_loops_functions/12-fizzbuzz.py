d232c6dddc97774ff250dfd0b40abdb18ee77e2a
edwinogwel <edogwel@gmail.com>
2022-06-01 00:31:56 +0300

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         message = ""
-        if (i % 3 == 0) and (i % 5 == 0):
+        if i % 15 == 0:
             message = "FizzBuzz"
         elif i % 3 == 0:
             message = "Fizz"

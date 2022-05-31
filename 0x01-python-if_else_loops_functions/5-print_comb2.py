a8286b8475a69d9b63848893990124f777ef696c
edwinogwel <edogwel@gmail.com>
2022-05-31 20:09:16 +0000

    Use .format() method approach for formatting strings

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(100):
-    if number == 99:
-        print(number)
+for i in range(100):
+    if i == 99:
+        print(i)
     else:
-        print(f"{number:0>2d}", end=", ")
+        print("{:0>2d}".format(i), end=", ")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for number in range(9):
     for another in range(number + 1, 10):
         if number == 8:
-            print(f"{number}{another}")
+            print("{}{}".format(number, another))
         else:
-            print(f"{number}{another}", end=", ")
+            print("{}{}".format(number, another), end=", ")

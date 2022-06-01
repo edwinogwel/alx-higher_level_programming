928992f539c24b4223a0e18010b2e1c8958a27c8
edwinogwel <edogwel@gmail.com>
2022-06-01 10:40:51 +0300

    Clean up code

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         message = ""
-        if i % 15 == 0:
+        if (i % 3 == 0) and (i % 5 == 0):
             message = "FizzBuzz"
         elif i % 3 == 0:
             message = "Fizz"
         elif i % 5 == 0:
             message = "Buzz"
-        print(message, end=" ") if message else print(i, end=" ")           
+        print(message, end=" ") if message else print(i, end=" ")

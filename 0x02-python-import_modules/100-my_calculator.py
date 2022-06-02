cee434dadccaeb9958f51577b4503c6c8355f349
edogwel <edogwel@gmail.com>
2022-06-03 00:12:57 +0300

    Clean implementation using dictionaries

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,24 +3,17 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
 
-    nArgs = len(argv)
-    if nArgs != 4:      # argv[0] -> program name
+    if len(argv) != 4:      # argv[0] -> program name
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    a = int(argv[1])    # 1st arg
-    op = argv[2]        # operator
-    b = int(argv[3])    # 2nd arg
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
 
-    message = ""
-    if op == '+':
-        message = f"{a} {op} {b} = {add(a, b)}"
-    if op == '-':
-        message = f"{a} {op} {b} = {sub(a, b)}"
-    if op == '*':
-        message = f"{a} {op} {b} = {mul(a, b)}"
-    if op == '/':
-        message = f"{a} {op} {b} = {div(a, b)}"
+    if op not in ops.keys():
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
-    print(message) if message else print(
-        "Unknown operator. Available operators: +, -, * and /") & exit(1)
+    print(f"{a} {op} {b} = {ops[op](a, b)}")

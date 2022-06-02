a05e69f7ec50d265f9e9c71d79f71c346279bfa2
edogwel <edogwel@gmail.com>
2022-06-02 22:21:44 +0300

    Add advanced tasks

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    nArgs = len(argv)
+    if nArgs != 4:      # argv[0] -> program name
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])    # 1st arg
+    op = argv[2]        # operator
+    b = int(argv[3])    # 2nd arg
+
+    if op is '+':
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
+    elif op is '-':
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+    elif op is '*':
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+    elif op is '/':
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)

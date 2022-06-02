1311a0c1d9d8fa6c359a35f8a349e3cde6e7fb34
edwinogwel <edogwel@gmail.com>
2022-06-02 13:08:04 +0000

    Add task 0-2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = len(sys.argv)
+    if args == 1:
+        print(f"{args - 1} arguments.")
+    elif args == 2:
+        print(f"{args - 1} argument:")
+    else:
+        print(f"{args - 1} arguments:")
+
+    for i in range(1, args):
+        print(f"{i}: {sys.argv[i]}")

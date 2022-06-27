7ceca5a89c189b04e7aa8c174507d7435fe6ab98
Edwin Ian Ogwel <edogwel@gmail.com>
2022-06-27 23:55:33 +0300

    Add @property and .setter decorators

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,26 +10,30 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    @property
     def width(self):
         """ Get width. """
-        return self.width
+        return self.__width
 
+    @width.setter
     def width(self, value):
         """ Set width. """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.width = value
+        self.__width = value
 
+    @property
     def height(self):
         """ Get height. """
-        return self.height
+        return self.__height
 
+    @height.setter
     def height(self, value):
         """ Set height. """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.height = value
+        self.__height = value

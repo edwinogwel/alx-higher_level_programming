02ed759ae7505e5a13518ca4ecd64743e4c9802c
Edwin Ian Ogwel <edogwel@gmail.com>
2022-06-29 18:55:57 +0300

    Correct task

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -8,5 +8,5 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size > 0:
+    if size >= 0:
         print(*(['#' * size] * size), sep='\n')

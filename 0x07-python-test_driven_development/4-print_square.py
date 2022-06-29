17f1a182fc09161bf460f50b25c3df0588c357cc
Edwin Ian Ogwel <edogwel@gmail.com>
2022-06-29 18:53:51 +0300

    Correct task

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,8 +4,9 @@
 
 def print_square(size):
     " Print the square with the character #. "
-    if type(size) is not int: 
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    if size <= 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
-    print(*(["#" * size] * size), sep="\n")
+    if size > 0:
+        print(*(['#' * size] * size), sep='\n')

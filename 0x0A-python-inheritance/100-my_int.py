a628a699e1055aa8cc9757f93ffb4e0ee875bc37
Edwin Ian Ogwel <edogwel@gmail.com>
2022-07-04 23:06:33 +0300

    Add solutions for task 8-13

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class MyInt(int):
+    """
+    MyInt inherits from int, but reverses
+    the behavior of != and ==
+    """
+
+    def __eq__(self, value):
+        """ Return not equal instead of equal """
+        return super().__ne__(value)
+
+    def __ne__(self, value):
+        """ Return equal instead of not equal """
+        return super().__eq__(value)
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This module defines a class Square """
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Square that inherits from Rectangle """
+
+    def __init__(self, size):
+        """ Instantiation with width """
+        try:
+            super().__init__(size, size)
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise TypeError("size must be greater than 0")
+        self.__size = size
+
+    def __str__(self):
+        """ Return description of the square """
+        return f"[Square] {self.__size}/{self.__size}"

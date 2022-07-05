8d988484b48aea5869fe9a754f7e84e8fba9b51b
Edwin Ian Ogwel <edogwel@gmail.com>
2022-07-05 18:20:20 +0300

    Add solutions for task 1-4

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """ Provides a function to work with files """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ Append text to a file """
     with open(filename, "a") as file:
         return file.write(text)
